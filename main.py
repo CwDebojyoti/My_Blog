c30252381272c6611d1b0575550b39659f78d9f5
Debojyoti Chattoraj <debojyotichattoraj1996@gmail.com>
2026-02-27 22:25:55 +0530

    mentioned local host port

--- main.py
+++ main.py
@@ -428,4 +428,4 @@ def send_message(name, user_email, phone, message):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host= '0.0.0.0')
+    app.run(debug=True, host= '0.0.0.0', port= 8000)
